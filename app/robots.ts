6576ca4ebdad3d26045191c0a74823771084302d
yunseok91 <dldbstjr911105@gmail.com>
2026-05-29 21:49:57 +0900

    사이트맵수정

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
 
-const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://voyalogue.com'
+const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://voyalogue.vercel.app'
 
 export default function robots(): MetadataRoute.Robots {
   return {
